3965b1335df97469d2696cb23c64a38bf5a79d71
LoganArnett <Loganarn5@gmail.com>
2016-03-01 19:12:45 -0500

    updating for CORS just in case

--- server.js
+++ server.js
@@ -3,10 +3,34 @@
 var express = require('express'),
     app = express(),
     mongoose = require('mongoose'),
-    db = mongoose.connect('mongodb://localhost/dat-ass');
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost/dat-ass');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(methodOverride('X-HTTP-Method-Override'));
+var CORS = function(req, res, next) {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'Content-Type');
+if ('OPTIONS' === req.method) {
+  res.send(200);
+}
+else {
+  next();
+}
+};
+app.use(CORS);
 
 app.get('/', function (req, res) {
-    res.send('Check out Dat Ass!!!')
+    res.send('Check out Dat Ass!!!');
 });
 
 
